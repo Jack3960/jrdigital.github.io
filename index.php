10135e7aae8e15f5c3d8b31c4e28f926ffb7dfdb
Jack3960 <53895360+Jack3960@users.noreply.github.com>
2019-08-13 09:39:25 +0530

    nice

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 		<h1>Rock Band</h1>
 		<nav>
 			<ul>
-				<li><a class="activ" href="index.html">HOME</a></li>
-				<li><a class="noactiv" href="video.html">VIDEOS</a></li>
-				<li><a class="noactiv" href="about.html">ABOUT</a></li>
-				<li><a class="noactiv" href="contact.html">CONTECT</a></li>
+				<li><a class="activ" href="index.phpphpphpphp">HOME</a></li>
+				<li><a class="noactiv" href="video.phpphpphp">VIDEOS</a></li>
+				<li><a class="noactiv" href="about.phpphp">ABOUT</a></li>
+				<li><a class="noactiv" href="contact.php">CONTECT</a></li>
 			</ul>
 		</nav>
 	</header>
